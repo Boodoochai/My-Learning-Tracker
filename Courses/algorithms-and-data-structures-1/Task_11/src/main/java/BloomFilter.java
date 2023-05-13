fd202d7646e3c6d36193b88e48ad066ab03084a9
Boodoochai <itstoomymail@mail.ru>
2023-05-13 22:38:07 +0500

    Doing courses: Completed new task.

--- Courses/algorithms-and-data-structures-1/Task_11/src/main/java/BloomFilter.java
+++ Courses/algorithms-and-data-structures-1/Task_11/src/main/java/BloomFilter.java
@@ -1,39 +1,46 @@
 public class BloomFilter
 {
       public int filter_len;
+      private int bit_arr;
 
       public BloomFilter(int f_len)
       {
-        filter_len = f_len;
-        // создаём битовый массив длиной f_len ...
+            filter_len = f_len;
+            bit_arr = 0;
       }
 
-      // хэш-функции
       public int hash1(String str1)
       {
-        // 17
-        for(int i=0; i<str1.length(); i++)
-        {
-            int code = (int)str1.charAt(i);
-        }
-        // реализация ...
-        return 0;
+            int hash = 0;
+            for(int i=0; i<str1.length(); i++)
+            {
+                  int code = (int)str1.charAt(i);
+                  hash = (hash * 17 + code) % filter_len;
+            }
+            return hash;
       }
+
       public int hash2(String str1)
       {
-        // 223
-        // реализация ...
-        return 0;
+            int hash = 0;
+            for(int i=0; i<str1.length(); i++)
+            {
+                  int code = (int)str1.charAt(i);
+                  hash = (hash * 223 + code) % filter_len;
+            }
+            return hash;
       }
 
       public void add(String str1)
       {
-        // добавляем строку str1 в фильтр
+            bit_arr |= 1 << hash1(str1);
+            bit_arr |= 1 << hash2(str1);
       }
 
       public boolean isValue(String str1)
       {
-        // проверка, имеется ли строка str1 в фильтре
-        return false;
+            if ((1 << hash1(str1) & bit_arr) != 0 && (1 << hash2(str1) & bit_arr) != 0)
+                  return true;
+            return false;
       }
 }
--- Courses/algorithms-and-data-structures-1/Task_11/src/test/java/BloomFilterTest.java
+++ Courses/algorithms-and-data-structures-1/Task_11/src/test/java/BloomFilterTest.java
@@ -2,4 +2,52 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class BloomFilterTest {
+  @Test
+  public void hash1Test() {
+    BloomFilter filter = new BloomFilter(32);
+
+    Assert.assertEquals(filter.hash1("a"), 1);
+    Assert.assertEquals(filter.hash1("b"), 2);
+    Assert.assertEquals(filter.hash1("G"), 7);
+    Assert.assertEquals(filter.hash1("aa"), 18);
+    Assert.assertEquals(filter.hash1("jd"), 14);
+  }
+
+  @Test
+  public void hash2Test() {
+    BloomFilter filter = new BloomFilter(32);
+
+    Assert.assertEquals(filter.hash2("a"), 1);
+    Assert.assertEquals(filter.hash2("b"), 2);
+    Assert.assertEquals(filter.hash2("G"), 7);
+    Assert.assertEquals(filter.hash2("aa"), 0);
+    Assert.assertEquals(filter.hash2("Ha"), 25);
+  }
+
+  @Test
+  public void isValueTest() {
+    BloomFilter filter = new BloomFilter(32);
+
+    filter.add("0123456789");
+    filter.add("1234567890");
+    filter.add("2345678901");
+    filter.add("3456789012");
+    filter.add("4567890123");
+    filter.add("5678901234");
+    filter.add("6789012345");
+    filter.add("7890123456");
+    filter.add("8901234567");
+    filter.add("9012345678");
+
+    Assert.assertTrue(filter.isValue("0123456789"));
+    Assert.assertTrue(filter.isValue("1234567890"));
+    Assert.assertTrue(filter.isValue("2345678901"));
+    Assert.assertTrue(filter.isValue("3456789012"));
+    Assert.assertTrue(filter.isValue("4567890123"));
+    Assert.assertTrue(filter.isValue("5678901234"));
+    Assert.assertTrue(filter.isValue("6789012345"));
+    Assert.assertTrue(filter.isValue("7890123456"));
+    Assert.assertTrue(filter.isValue("8901234567"));
+    Assert.assertTrue(filter.isValue("9012345678"));
+  }
 }
