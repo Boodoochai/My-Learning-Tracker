4dc081ff60b7f2457ef941f4ee55fdd014ed300e
Boodoochai <itstoomymail@mail.ru>
2023-05-20 11:10:55 +0500

    Doing LeetCode: Started new task and wrote some tests.

---
+++ LeetCode/Evaluate_Division/src/main/java/Solution.java
@@ -0,0 +1,8 @@
+import java.util.List;
+
+class Solution {
+  public double[] calcEquation(List<List<String>> equations, double[] values, List<List<String>> queries) {
+    double[] res = new double[0];
+    return res;
+  }
+}
