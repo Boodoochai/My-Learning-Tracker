380714c4157233c26a05418617b085d25f7de7b5
Boodoochai <itstoomymail@mail.ru>
2023-05-21 09:25:14 +0500

    Doing LeetCode: solved new task.

--- LeetCode/Evaluate_Division/src/test/java/SolutionTest.java
+++ LeetCode/Evaluate_Division/src/test/java/SolutionTest.java
@@ -5,6 +5,64 @@ import org.junit.Assert;
 
 public class SolutionTest {
   @Test
+  public void testCalcEquationTwoNodesQueriTwoSameNodes() {
+    Solution solution = new Solution();
+    List<List<String>> equations = new ArrayList<>();
+    equations.add(new ArrayList<String>());
+    equations.get(0).add("a");
+    equations.get(0).add("b");
+    List<List<String>> queris = new ArrayList<>();
+    queris.add(new ArrayList<String>());
+    queris.get(0).add("a");
+    queris.get(0).add("a");
+    queris.add(new ArrayList<String>());
+    queris.get(1).add("b");
+    queris.get(1).add("b");
+    double[] values = { 2 };
+    double[] ans = { 1.0, 1.0 };
+
+    double[] res = solution.calcEquation(equations, values, queris);
+
+    Assert.assertEquals(ans[0], res[0], 0.1);
+    Assert.assertEquals(ans[1], res[1], 0.1);
+  }
+
+  @Test
+  public void test4() {
+    Solution solution = new Solution();
+    List<List<String>> equations = new ArrayList<>();
+    equations.add(new ArrayList<String>());
+    equations.get(0).add("a");
+    equations.get(0).add("b");
+    List<List<String>> queris = new ArrayList<>();
+    queris.add(new ArrayList<String>());
+    queris.get(0).add("a");
+    queris.get(0).add("b");
+    queris.add(new ArrayList<String>());
+    queris.get(1).add("b");
+    queris.get(1).add("a");
+    queris.add(new ArrayList<String>());
+    queris.get(2).add("a");
+    queris.get(2).add("a");
+    queris.add(new ArrayList<String>());
+    queris.get(3).add("b");
+    queris.get(3).add("b");
+    queris.add(new ArrayList<String>());
+    queris.get(4).add("x");
+    queris.get(4).add("r");
+    double[] values = { 2 };
+    double[] ans = { 2.0, 0.5, 1.0, 1.0, -1.0 };
+
+    double[] res = solution.calcEquation(equations, values, queris);
+
+    Assert.assertEquals(ans[0], res[0], 0.1);
+    Assert.assertEquals(ans[1], res[1], 0.1);
+    Assert.assertEquals(ans[2], res[2], 0.1);
+    Assert.assertEquals(ans[3], res[3], 0.1);
+    Assert.assertEquals(ans[4], res[4], 0.1);
+  }
+
+  @Test
   public void test1() {
     Solution solution = new Solution();
     List<List<String>> equations = new ArrayList<>();
@@ -35,11 +93,11 @@ public class SolutionTest {
 
     double[] res = solution.calcEquation(equations, values, queris);
 
-    Assert.assertEquals(ans[0], res[0], 0);
-    Assert.assertEquals(ans[1], res[1], 0);
-    Assert.assertEquals(ans[2], res[2], 0);
-    Assert.assertEquals(ans[3], res[3], 0);
-    Assert.assertEquals(ans[4], res[4], 0);
+    Assert.assertEquals(ans[0], res[0], 0.1);
+    Assert.assertEquals(ans[1], res[1], 0.1);
+    Assert.assertEquals(ans[2], res[2], 0.1);
+    Assert.assertEquals(ans[3], res[3], 0.1);
+    Assert.assertEquals(ans[4], res[4], 0.1);
   }
 
   @Test
@@ -89,7 +147,7 @@ public class SolutionTest {
     List<List<String>> queris = new ArrayList<>();
     queris.add(new ArrayList<String>());
     queris.get(0).add("a");
-    queris.get(0).add("c");
+    queris.get(0).add("b");
     queris.add(new ArrayList<String>());
     queris.get(1).add("b");
     queris.get(1).add("a");
