be15fd02e80d4b6b284e4e0214ad6da825bcbd5b
Boodoochai <itstoomymail@mail.ru>
2023-08-22 22:13:26 +0500

    Doing Courses: complited new task.

---
+++ Courses/SQL-from-scratch/Task_12/records_update.sql
@@ -0,0 +1,7 @@
+INSERT INTO (EmployeeID, LastName, FirstName, Title, TItleOfCourtesy, BirthDate, HireDate, City, Region, PostalCode, Country, HomePhone, Extension, Notes, ReportsTo)
+VALUES (10, 'Ivan','Ivanov','Sales Representative','Mr.','2000-01-01 00:00:00','2022-01-01 00:00:00','Moscow','WA','98122','Russia','(333) 222-4477', 8,'Education includes a BA in psychology from Ural FEderal Universary in 2020','2');
+
+INSERT INTO EmployeeTerritories VALUES (10,6897);
+
+INSERT INTO Orders VALUES (1024899,'VINET',10,'1996-07-04 00:00:00','1996-08-01 00:00:00','1996-07-16 00:00:00',3,32.3800,'Vins et alcools Chevalier','59 rue de l\'Abbaye','Reims','','51100','France');
+// Нет, не возникнут.
